e2201003763f96fab112a17b276d92adc16bb0d0
mkikolski <261830@student.pwr.edu.pl>
2025-05-12 09:21:15 +0200

    added more logging statements

--- run.py
+++ run.py
@@ -13,12 +13,16 @@ args = parser.parse_args()
 if __name__ == "__main__":
     p = Pipeline()
     if args.step == "fetch":
+        print("Entered fetch step")
         p.add_step({"name": "Fetching pretraining data", "function": Steps.fetch_data})
     elif args.step == "pretrain":
+        print("Entered pretraining step")
         p.add_step({"name": "Pretraining generator", "function": Steps.pretrain_generator})
     elif args.step == "dqn":
+        print("Entered dqn training step")
         p.add_step({"name": "Training DQN", "function": Steps.train_dqn})
     elif args.full:
+        print("Executing full pipeline")
         p.add_step({"name": "Fetching pretraining data", "function": Steps.fetch_data})\
             .add_step({"name": "Pretraining generator", "function": Steps.pretrain_generator})\
             .add_step({"name": "Training DQN", "function": Steps.train_dqn})
